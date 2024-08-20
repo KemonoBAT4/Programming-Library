b05c73b3dad4c0f22bedad065b9d660c0493156d
marco-battisti-04 <156814368+marco-battisti-04@users.noreply.github.com>
2024-08-20 15:19:02 +0000

    added scripts and dependencies for the application

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 23104;
+
+// TODO: install nodemom, ejs
+
+
+
+
+app.listen(port, ()=> {
+    console.log(`listening on port: ${port}`)
+})
+
