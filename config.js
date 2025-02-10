a3320a5814ebb09e8eec17a405af9010728e7210
KemonoBAT4 <kemono.bat.4@gmail.com>
2025-02-10 23:30:47 +0100

    added scripts

--- config.js
+++ config.js
@@ -1,3 +1,14 @@
 module.exports = {
-    port: 23104
+    // Application
+    port: 23104,
+
+    // Api
+    api_version: 1,
+
+    // Database
+    sql_host: "localhost",
+    sql_user: "root",
+    sql_password: "root",
+    sql_port: 3306,
+    sql_database: "ProgrammingLibrary"
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const api = express.Router();
+const mysql = require("mysql");
+let config = {} // FIXME: fix config, import config file
+
+const connection = mysql.createConnection(
+    {
+        host: `${config.sql_host}`,
+        user: `${config.sql_user}`,
+        password: `${config.sql_password}`,
+        database: `${config.sql_database}`,
+        port: config.sql_port
+    }
+);
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 // redirect to home page
 api.get("/", async(req, res) => {
