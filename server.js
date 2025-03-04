2b688155a83cd762641eab1653bebff565a5d4cd
KemonoBAT4 <kemono.bat.4@gmail.com>
2025-03-04 20:36:12 +0100

    deleted useless files, updated scripts location

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const { port } = require("./config");
+const { port } = require("./src/settings/config");
 
 // Application
 const app = express();
